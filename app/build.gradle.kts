5ce609d556a12103c68383f3f64b25307ba95038
Simona Hristova <11033394+sstoyanova@users.noreply.github.com>
2025-05-13 09:30:53 +0300

    fix git configuration and add missing files

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.nanit.happywebsocketbirthday"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.nanit.happywebsocketbirthday"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    // Ktor Core
+    implementation(libs.ktor.client.core) // Use the latest version
+    // Ktor Content Negotiation (for JSON)
+    implementation(libs.ktor.client.content.negotiation)
+    // Ktor JSON (Gson or kotlinx.serialization)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.websockets) // Check for latest version
+    // Ktor CIO Engine
+    implementation(libs.ktor.client.cio)
+    // Ktor Logging
+    implementation(libs.ktor.client.logging)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.androidx.navigation.compose)
+    // Hilt for Compose Navigation
+    implementation(libs.androidx.hilt.navigation.compose)
+    // Coil for image loading in Compose
+    implementation(libs.coil.compose)
+
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,82 +1,9 @@
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization")
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
-}
-
-android {
-    namespace = "com.nanit.happywebsocketbirthday"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.nanit.happywebsocketbirthday"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
-
-dependencies {
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-    // Ktor Core
-    implementation(libs.ktor.client.core) // Use the latest version
-    // Ktor Content Negotiation (for JSON)
-    implementation(libs.ktor.client.content.negotiation)
-    // Ktor JSON (Gson or kotlinx.serialization)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.client.websockets) // Check for latest version
-    // Ktor CIO Engine
-    implementation(libs.ktor.client.cio)
-    // Ktor Logging
-    implementation(libs.ktor.client.logging)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.kotlinx.datetime)
-    implementation(libs.androidx.navigation.compose)
-    // Hilt for Compose Navigation
-    implementation(libs.androidx.hilt.navigation.compose)
-    // Coil for image loading in Compose
-    implementation(libs.coil.compose)
-
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    kotlin("plugin.serialization") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
